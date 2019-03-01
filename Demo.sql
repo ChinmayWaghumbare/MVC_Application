754a54bead23788c907c0b1cf309c6964e8aea48
Chinmay Waghumbare <chinmayw@corp.fdsinfotech.com>
2019-03-01 17:51:59 +0530

    Add project files.

---
+++ Demo.sql
@@ -0,0 +1,3 @@
+﻿/*
+No database-level extended properties were found or all existing extended properties are open in other windows
+*/create table A (ID int primary key,Name nchar(20))
---
+++ MVC_Application/Controllers/AController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Application.Controllers
+{
+    public class AController : Controller
+    {
+        // GET: A
+        public ActionResult CreateA()
+        {
+            return View();
+        }
+    }
+}
